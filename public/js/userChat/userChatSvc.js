58cef78b5d81b9f96e5fc943fcd9d0a978ad7f77
Cory Figueroa <c.t.figueroa@gmail.com>
2014-09-11 16:33:20 -0400

    added $http events in the userChatSvc file

--- public/js/userChat/userChatSvc.js
+++ public/js/userChat/userChatSvc.js
@@ -3,12 +3,36 @@ angular.module('userChatApp')
 	.factory('userChatSvc', function($log, $rootScope, $http) {
 
 		var userName = '';
+		var chatsUrl = 'api/collections/chatappironyard';
+
+		var getChats = function() {
+
+			$http.get(chatsUrl).success(function(data) {
+
+				return data;
+
+			});
+
+		var addChat = function(chat) {
+
+			$http.post(chatsUrl, chat).success(function (data) {
+
+				$rootScope.$broadcast('chat:added');
+				$log.info('chat:added');
+
+			});
+
+		};
+
+		};
 
 		return {
 
-			userName:userName
+			userName:userName,
+			getChats:getChats,
+			addChat:addChat
 
-		}
+		};
 
 
 	});
